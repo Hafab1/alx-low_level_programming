976da52f82d9f347df6b5c631e13fb8248376998
hafida <h.abourrah@gmail.com>
2023-04-20 15:17:39 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include < stdio.h>
+/**
+ * main - A program that pritns using the function puts
+ * Retuen: 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
